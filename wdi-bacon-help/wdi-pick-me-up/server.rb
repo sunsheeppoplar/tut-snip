ec8e4f93d7a325e3455b189fdb1e735b1690213f
Abner Yang <abtyang@gmail.com>
2015-10-29 00:57:44 -0400

    grabbing info from params for ERB

--- wdi-bacon-help/wdi-pick-me-up/server.rb
+++ wdi-bacon-help/wdi-pick-me-up/server.rb
@@ -2,11 +2,13 @@ module ComplimentApp
 
 	class Server < Sinatra::Base
 
-		get '/' do
+		get '/next-a-name/:name/then-a-number/:number' do
 		@compliments = Compliments::WORD_LIST
+		@name = params[:name]
+		@number = params[:number]
 
 		erb :index
-		end # / route
+		end # route
 
 	end # Server
 end # ComplimentApp
