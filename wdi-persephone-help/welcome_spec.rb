a7d4fce3722bf106dad3100900523119a3b97027
Abner Yang <abtyang@gmail.com>
2015-12-10 21:02:18 -0500

    index page expectations

--- wdi-persephone-help/welcome_spec.rb
+++ wdi-persephone-help/welcome_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-describe "navigation on homepage" do
+describe "homepage features" do
 	it "has link to artist index" do
-		
+		visit "/"
+		expect(page).to have_css("a", :href => "/artists")
+	end
+
+	it "has link to paintings index" do
+		visit "/"
+		expect(page).to have_css("a", :href => "/paintings")
+	end
+
+	it "can navigate to artist index" do
+		visit "/"
+		index = page.find("a", :href => "/artists")
+		click_link(index)
+		expect(current_path).to eq("/artists")
+	end
+
+	it "can navigate to paintings index" do
+		visit "/"
+		index = page.find("a", :href => "/paintings")
+		click_link(index)
+		expect(current_path).to eq("/paintings")
+	end
+
+end
